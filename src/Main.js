0a35ce2baa213a6928982119c1b3af4acce814a2
Sota Makino <sm96kyoto@gmail.com>
2018-12-24 17:55:32 +0900

    publish version 1.0

--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import title from './assets/main/main_title.png';
+import main1 from './assets/main/main1.jpg';
+import main2 from './assets/main/main2.jpg';
+import main3 from './assets/main/main3.jpg';
+import main4 from './assets/main/main4.jpg';
+import main5 from './assets/main/main5.jpg';
+import main6 from './assets/main/main6.jpg';
+
 import './Main.css';
 
 export default class Main extends React.Component {
     render(){
         return(
             <div>
-                <title>大集合</title>
-                <nav>
-                    <ul>
-                        <li><Link to="/">HOME</Link></li>
-                        <li><Link to="/benizake">ベニザケ</Link></li>
-                        <li><Link to="/masuno">マスノザケ</Link></li>
-                        <li><Link to="/karahuto">カラフト</Link></li>
-                        <li><Link to="/sakura">サクラ</Link></li>
-                        <li><Link to="/sirozake">しろ</Link></li>
-                        <li><Link to="/ginzake">銀鮭</Link></li>
+                <div class="title">
+                    <img src={title} alt="" class="main_title"/>
+                </div>
+                <div class="fishes">
+                    <ul class="menu">
+                        <li><Link to="/sakura"><img src={main1} alt="" class="fish_link"/></Link></li>
+                        <li><Link to="/benizake"><img src={main2} alt="" class="fish_link"/></Link></li>
+                        <li><Link to="/masuno"><img src={main3} alt="" class="fish_link"/></Link></li>
                     </ul>
-                </nav>
+                    <ul class="menu">
+                        <li><Link to="/sirozake"><img src={main4} alt="" class="fish_link"/></Link></li>
+                        <li><Link to="/karahuto"><img src={main5} alt="" class="fish_link"/></Link></li>
+                        <li><Link to="/ginzake"><img src={main6} alt="" class="fish_link"/></Link></li>
+                    </ul>
+                </div>
             </div>
         )
     }
