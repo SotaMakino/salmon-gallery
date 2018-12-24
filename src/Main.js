d5468eff257bcffc4d92013e598237913fa1c19f
Sota Makino <sm96kyoto@gmail.com>
2018-12-24 20:51:02 +0900

    version 1.1

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import title from './assets/Main/main_title.png';
 import main1 from './assets/Main/main1.jpg';
 import main2 from './assets/Main/main2.jpg';
 import main3 from './assets/Main/main3.jpg';
@@ -15,19 +14,16 @@ export default class Main extends React.Component {
     render(){
         return(
             <div>
-                <div class="title">
-                    <img src={title} alt="" class="main_title"/>
-                </div>
                 <div class="fishes">
                     <ul class="menu">
-                        <li><Link to="/sakura"><img src={main1} alt="" class="fish_link"/></Link></li>
+                        <li><Link to="/sirozake"><img src={main4} alt="" class="fish_link"/></Link></li>
                         <li><Link to="/benizake"><img src={main2} alt="" class="fish_link"/></Link></li>
-                        <li><Link to="/masuno"><img src={main3} alt="" class="fish_link"/></Link></li>
+                        <li><Link to="/ginzake"><img src={main6} alt="" class="fish_link"/></Link></li>
                     </ul>
                     <ul class="menu">
-                        <li><Link to="/sirozake"><img src={main4} alt="" class="fish_link"/></Link></li>
+                        <li><Link to="/sakura"><img src={main1} alt="" class="fish_link"/></Link></li>
                         <li><Link to="/karahuto"><img src={main5} alt="" class="fish_link"/></Link></li>
-                        <li><Link to="/ginzake"><img src={main6} alt="" class="fish_link"/></Link></li>
+                        <li><Link to="/masuno"><img src={main3} alt="" class="fish_link"/></Link></li>
                     </ul>
                 </div>
             </div>
