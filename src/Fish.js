769ad03e1b2db16ee6cfcc14810e78149b3e39bc
Sota Makino <sm96kyoto@gmail.com>
2019-05-01 16:40:03 +0900

    feel like I understood props

---
+++ src/Fish.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Slider from 'react-rangeslider';
+import { Link } from 'react-router-dom';
+
+import './react-rangeslider/lib/index.css';
+import './Fish.css';
+
+import sea from './assets/interface/sea.png';
+import river from './assets/interface/river.png';
+import title from './assets/interface/title_beni.png';
+import back from './assets/interface/back.png';
+
+
+export default class Fish extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      volume: 0
+    }
+  }
+ 
+  handleOnChange = (value) => {
+    this.setState({
+      volume: value
+    })
+  }
+ 
+  render() {
+    const { volume } = this.state;
+
+    const image1 = this.state.volume >= 0 && this.state.volume < 10 ? <img src={this.props.images[0]} alt="" class="fish" /> : '';
+    const image2 = this.state.volume >= 10 && this.state.volume < 20 ? <img src={this.props.images[1]} alt="" class="fish" /> : '';
+    const image3 = this.state.volume >= 20 && this.state.volume < 30 ? <img src={this.props.images[2]} alt="" class="fish" /> : '';
+    const image4 = this.state.volume >= 30 && this.state.volume < 40 ? <img src={this.props.images[3]} alt="" class="fish" /> : '';
+    const image5 = this.state.volume >= 40 && this.state.volume < 50 ? <img src={this.props.images[4]} alt="" class="fish" /> : '';
+    const image6 = this.state.volume >= 50 && this.state.volume < 60 ? <img src={this.props.images[5]} alt="" class="fish" /> : '';
+    const image7 = this.state.volume >= 60 && this.state.volume <= 70 ? <img src={this.props.images[6]} alt="" class="fish" /> : '';
+
+
+    return (
+      <div>
+        <div>
+          <img src={title} alt='' class="title" />
+        </div>
+        <div>
+          <Link to="/"><img src={back} alt='' class="back"/></Link>
+        </div>
+        <div>
+          {image1}
+          {image2}
+          {image3}
+          {image4}
+          {image5}
+          {image6}
+          {image7}
+        </div>
+        <div>
+          <img src={sea} alt='' class="sea"/>
+          <img src={river} alt='' class="river"/>
+        </div>
+          <div className="bar">
+            <Slider
+              value={volume}
+              max={70}
+              tooltip={false}
+              onChange={this.handleOnChange}
+            />
+          </div>
+      </div>
+    )
+  }
+}
--- src/Ginzake.js
+++ src/Ginzake.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import Slider from 'react-rangeslider';
-import { Link } from 'react-router-dom';
-
-import './react-rangeslider/lib/index.css';
-import './Fish.css';
+import Fish from './Fish';
 
 import pic1 from './assets/ginzake/1.jpg';
 import pic2 from './assets/ginzake/2.jpg';
@@ -12,72 +8,15 @@ import pic4 from './assets/ginzake/4.jpg';
 import pic5 from './assets/ginzake/5.jpg';
 import pic6 from './assets/ginzake/6.jpg';
 import pic7 from './assets/ginzake/7.jpg';
-import sea from './assets/interface/sea.png';
-import river from './assets/interface/river.png';
-import title from './assets/interface/title_gin.png';
-import back from './assets/interface/back.png';
 
+const ginImages = [pic1, pic2, pic3, pic4, pic5, pic6, pic7]
 
-export default class Ginzake extends React.Component {
-
-  constructor() {
-    super()
-    this.state = {
-      volume: 0
-    }
-  }
- 
-  handleOnChange = (value) => {
-    this.setState({
-      volume: value
-    })
-  }
-
-  componentWillMount() {
-  }
- 
-  render() {
-    const { volume } = this.state;
-
-    const image1 = this.state.volume >= 0 && this.state.volume < 10 ? <img src={pic1} alt="" class="fish" /> : '';
-    const image2 = this.state.volume >= 10 && this.state.volume < 20 ? <img src={pic2} alt="" class="fish" /> : '';
-    const image3 = this.state.volume >= 20 && this.state.volume < 30 ? <img src={pic3} alt="" class="fish" /> : '';
-    const image4 = this.state.volume >= 30 && this.state.volume < 40 ? <img src={pic4} alt="" class="fish" /> : '';
-    const image5 = this.state.volume >= 40 && this.state.volume < 50 ? <img src={pic5} alt="" class="fish" /> : '';
-    const image6 = this.state.volume >= 50 && this.state.volume < 60 ? <img src={pic6} alt="" class="fish" /> : '';
-    const image7 = this.state.volume >= 60 && this.state.volume <= 70 ? <img src={pic7} alt="" class="fish" /> : '';
-
-
-    return (
-      <div>
-        <div>
-          <img src={title} alt='' class="title" />
-        </div>
-        <div>
-          <Link to="/"><img src={back} alt='' class="back"/></Link>
-        </div>
-        <div>
-          {image1}
-          {image2}
-          {image3}
-          {image4}
-          {image5}
-          {image6}
-          {image7}
-        </div>
-        <div>
-          <img src={sea} alt='' class="sea"/>
-          <img src={river} alt='' class="river"/>
-        </div>
-          <div className="bar">
-            <Slider
-              value={volume}
-              max={70}
-              tooltip={false}
-              onChange={this.handleOnChange}
-            />
-          </div>
-      </div>
-    )
-  }
+function renderFishImages() {
+  return(
+    <Fish 
+      images={ginImages}
+    />
+  )
 }
+
+export default renderFishImages;
