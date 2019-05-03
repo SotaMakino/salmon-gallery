b7f7e42b9fe0a9240dcc12e76894322cbf244a05
Sota Makino <sm96kyoto@gmail.com>
2019-05-03 12:24:24 +0900

    fix mordal

--- src/Main.jsx
+++ src/Main.jsx
@@ -20,17 +20,11 @@ export default class Main extends React.Component {
     };
 
     this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
   openModal() {
     this.setState({ modalIsOpen: true });
-  }
-
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = "black";
   }
 
   closeModal() {
